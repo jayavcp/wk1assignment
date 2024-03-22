bdd106332c1884149f854e6674f9e245e061a1b0
JAYA CHEDUMBARUM PILLAY <jayavcp@gmail.com>
2024-03-22 17:00:38 +0000

    version with working buttons

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 console.log("hello universe");
+//selecting the elements by id
 const music1 = document.getElementById("music1");
+const playBtn = document.getElementById("playBtn");
+const stopBtn = document.getElementById("stopBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const backBtn = document.getElementById("backBtn");
+const fwBtn = document.getElementById("fwBtn");
+
+playBtn.addEventListener("click", function () {
+  music1.play();
+});
+
+stopBtn.addEventListener("click", function () {
+  music1.pause();
+  music1.currentTime = 0;
+});
+
+pauseBtn.addEventListener("click", function () {
+  music1.pause();
+});
+
+backBtn.addEventListener("click", function () {
+  music1.currentTime = music1.currentTime - 10;
+});
+
+fwBtn.addEventListener("click", function () {
+  music1.currentTime = music1.currentTime + 10;
+});
