dec5049abe5a0931a82884a3245580fe78222c66
JAYA CHEDUMBARUM PILLAY <jayavcp@gmail.com>
2024-03-24 21:58:24 +0000

    added in color button

--- app.js
+++ app.js
@@ -28,3 +28,31 @@ backBtn.addEventListener("click", function () {
 fwBtn.addEventListener("click", function () {
   music1.currentTime = music1.currentTime + 10;
 });
+
+// change color button
+const bgcolorButton = document.getElementById("color");
+let colorcount = 0;
+bgcolorButton.addEventListener("click", function () {
+  colorcount += 1;
+  if (colorcount % 7 == 1) {
+    document.body.style.backgroundColor = "red";
+  }
+  if (colorcount % 7 == 2) {
+    document.body.style.backgroundColor = "orange";
+  }
+  if (colorcount % 7 == 3) {
+    document.body.style.backgroundColor = "yellow";
+  }
+  if (colorcount % 7 == 4) {
+    document.body.style.backgroundColor = "green";
+  }
+  if (colorcount % 7 == 5) {
+    document.body.style.backgroundColor = "blue";
+  }
+  if (colorcount % 7 == 6) {
+    document.body.style.backgroundColor = "indigo";
+  }
+  if (colorcount % 7 == 0) {
+    document.body.style.backgroundColor = "purple";
+  }
+});
